67e1fc8f09c8a8f0186338c76cd4914c881df4db
firman-siahaan <firman.siahaan@gmail.com>
2026-04-19 00:16:30 +0700

    chapter-11: sending email

---
+++ ch11/01-multiple-recipients.js
@@ -0,0 +1,30 @@
+const nodemailer = require('nodemailer')
+
+const env = process.env.NODE_ENV || 'development'
+const credentials = require(`./.credentials.${env}.json`)
+
+const mailTransport = nodemailer.createTransport({
+  host: 'smtp.sendgrid.net',
+  auth: {
+    user: credentials.sendgrid.user,
+    pass: credentials.sendgrid.password,
+  },
+})
+
+async function go() {
+  try {
+    const result = await mailTransport.sendMail({
+      from: '"xxxxxx yyyyyyy - Meadowlark Travel" <xxxxxx.yyyyyyy@gmail.com>',
+      to: 'xxxxxx.yyyyyyy@hotmail.com, "Jae Customer" <jaeshambora@yahoo.com>, ' +
+        'xxxxxx.yyyyyyy@gmail.com',
+      subject: 'Your Meadowlark Travel Tour',
+      text: 'Thank you for booking your trip with Meadowlark Travel.  ' +
+        'We look forward to your visit!',
+    })
+    console.log('mail sent successfully: ', result)
+  } catch(err) {
+    console.log('could not send mail: ' + err.message)
+  }
+}
+
+go()
